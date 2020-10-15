367b157a0d2c7df14e690769da6ca8034fa0dcb2
6liuyu123 <1272635425@qq.com>
2020-10-15 09:51:14 +0800

    s0116 commit

---
+++ Tree/0116/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<Node> q = new LinkedList<Node>();
+        q.offer(root);
+        Node p, pre;
+        int i, n;
+        while (!q.isEmpty()) {
+            n = q.size();
+            for (i = 0; i < n; i++) {
+                p = q.poll();
+                if (i != 0) {
+                    pre.next = p;
+                }
+                pre = p;
+                if (p.left != null) {
+                    q.offer(p.left);
+                }
+                if (p.right != null) {
+                    q.offer(p.right);
+                }
+            }
+        }
+        return root;
+    }
+}
